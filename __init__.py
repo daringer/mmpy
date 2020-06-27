e8514d36cdf727e235184504076d75cc797c4179
Markus Meissner <coder@safemailbox.de>
2020-06-27 04:08:55 +0200

    auto endpoint generation based on function-signatures

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
-from .flask_simple_rest import get_rest_decorator, FlaskSimpleRest
 
-from .pwd_cls import Password
+from mmpy.flask_simple_rest import FlaskSimpleRest
+from mmpy.flask_simple_rest import get_rest_decorator, get_method_decorators
+
+from mmpy.pwd_cls import Password
--- flask_simple_rest.py
+++ flask_simple_rest.py
@@ -19,6 +19,10 @@ def my_post_endpoint():
 """
 
 import functools
+import inspect
+
+from pathlib import Path
+from traceback import extract_stack
 
 class FlaskSimpleRest:
     """
@@ -28,18 +32,94 @@ class FlaskSimpleRest:
       def my_view(foo, opt_arg=None):
           return jsonify({"some_data": 123})
     """
-    def __init__(self, flask_app):
+    def __init__(self, flask_app, auto_endpoint=False, endpoint_root=None):
         self.app = flask_app
+        self.root_path = None
+        self.type_map = {}
+
+        if auto_endpoint:
+            assert endpoint_root
+            self.root_path = Path(endpoint_root)
+            self.type_map = {
+                "int"                     : "int",
+                "float"                   : "float",
+                "str"                     : "string",
+                Path().__class__.__name__ : "path"
+            }
 
     def __getattr__(self, key):
         return self._wrap(key)
 
     def _wrap(self, method):
+        self.gen_endpoints(method)
+
         @functools.wraps(self.app.route)
         def func(*v, **kw):
             kw["methods"] = [method.upper()]
             return self.app.route(*v, **kw)
         return func
+
+    def gen_endpoints(self, wrapping_func):
+        stack = extract_stack()
+        my_call, others = stack[-1], stack[:-1]
+        assert len(others) > 0, "????"
+        rel_path = None
+        try:
+            rel_path = self.root_path.relative_to(external_caller.filename)
+        except ValueError as e:
+            print("HANDLE FAILING REL-PATH DETERMINATION")
+            raise e
+
+        endpoints = []
+
+        # traverse stack up until not-me file is found, its func is the caller
+        external_caller = None
+        while not external_caller:
+            caller = others.pop()
+            if caller.filename != my_call.filename:
+                external_caller = caller
+                break
+
+        # determine func-parameters to url-arguments mapping:
+        # v-args: add as 'path' in endpoint-url and ensure
+        #                        name + type-annotations are used
+        # kw-args: fork to multiple endpoints, regular one: w/o any default args.
+        #          next: one default arg, next: two ....
+        # kw-args (starting with "_"): will not be used for endpoint-url generation,
+        #                              (might be relevant for arg-skipping or POST data)
+        p_url, p_url_extra, p_hidden = [], [], []
+        sig = inspect.signature(wrapping_func)
+        for name, param in sig.parameters.items():
+            # keep hidden params and continue ...
+            if name.startswith("_"):
+                p_hidden.append(param)
+                continue
+
+            # construct url using name + type
+            _type = (self.type_map[param.annotation.__name__] + ":") \
+                        if param.annotation != inspect._empty else ""
+            box = (p_url if param.default == inspect._empty else p_url_extra)
+            box.append(f"<{_type}{name}>")
+        p_url_suffix = os.path.join(*p_url)
+        assert len(sig.parameters) == len(p_url) + len(p_url_extra) + len(p_hidden)
+
+        # get endpoint `basename` using `wrapping_func`'s (function)-name
+        funcname = wrapping_func.__name__
+        base_url = os.path.sep / rel_path.as_posix / funcname
+
+        # @TODO @FIXME, shall we have a special endpoint which might deliver info?
+        # would need specific decorator to mark the function to be used ....
+        #endpoints.append((base_ur)
+
+        # basic endpoint without any default parameters included in url
+        endpoints.append(base_url / p_url_suffix)
+        # further fork endpoints in ORDER of kw-params (params with defaults)
+        for idx, _ in enumerate(p_url_extra):
+            endpoints.append(base_url / p_url_suffix / os.path.join(*p_url_extra[:idx+1]))
+
+        print ("ENDPOINTS", "\n".join(endpoints))
+        return endpoints
+
 
 def get_rest_decorator(app):
     """get the main (rest)-decorator, examples: `mmpy.flask_simple_rest.__doc__`"""
@@ -53,6 +133,11 @@ def get_method_decorators(app, methods=None):
     methods = methods or ["get", "post", "put", "delete"]
     rest = FlaskSimpleRest(app)
     return [getattr(rest, meth) for meth in methods]
+
+
+
+
+
 
 
 # woho, brain-shrinking 8-liner, pythonic-fanciness including brain-damage
